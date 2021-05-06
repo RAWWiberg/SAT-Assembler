21928f00db7f88bc166349e9cf18077e63b56039
Malte Petersen <petersen@ie-freiburg.mpg.de>
2021-05-06 21:02:31 +0200

    bug fixes, semi-safe mode, and more information

--- SAT-Assembler.sh
+++ SAT-Assembler.sh
@@ -3,7 +3,8 @@
 # You may redistribute this software under the terms of GNU GENERAL PUBLIC LICENSE.
 # pipeline of MetaDomain. 
 
-set -e
+set -euo pipefail
+
 
 # input: 
 # -m: hmm file;
@@ -76,40 +77,51 @@ if [ "$out" == "" ];then
 fi
 
 
-if [ `which hmmsearch 2> /dev/null | wc -l` -eq 0 ]; then
+if ! which hmmsearch 2> /dev/null; then
   echo "hmmsearch is not found.";
   usage
-  exit
+  exit 1
 fi
 
-if [ `$DIR/check_python_packages.py` -eq 1 ];then
+if ! python $DIR/check_python_packages.py; then
   echo "Biopython or NetworkX is not found."
   usage
-  exit
-fi 
+  exit 1
+fi
 
-# generate a temporary folder.
+# create the output folder.
 if [ ! -d $out/ ];then
-  mkdir $out/
-else
-  echo 'Output folder exists. Please specify another output folder.'
-  exit
+  mkdir -p $out/
 fi
 tmp="$(cd $out && pwd)"
-base_fasta=`echo $fasta | awk '{split($1,a,"/"); print a[length(a)]}'`
+base_fasta="$(basename $fasta)"
 
-$DIR/DNA2Protein 1-6 $fasta $tmp/${base_fasta} 
+if [[ ! -f $tmp/${base_fasta}.frame1 ]]; then
+    echo "Translating reads into all six reading frames..."
+    $DIR/DNA2Protein 1-6 $fasta $tmp/${base_fasta}
+    echo "Done translating."
+fi
+
 # generate a list of domains in the input hmm file.
-python $DIR/parse_hmm_files.py $hmm $tmp/HMMs
-ls $tmp/HMMs | while read line
-do
-  hmm_acc=$(basename $line .hmm)
-  cat /dev/null >$tmp/${base_fasta}_${hmm_acc}.hmmer
-  for i in {1..6}
-  do
-   bash $DIR/hmmer3_pipeline_strand.sh $tmp/HMMs/$line $tmp/${base_fasta}.frame${i} $i >> $tmp/${base_fasta}_${hmm_acc}.hmmer
-  done
-  python $DIR/assembler.py $tmp/${base_fasta}_${hmm_acc}.hmmer $fasta ${hmm_acc} $t $d $out 
+if [[ ! -d $tmp/HMMs ]]; then
+    echo "Parsing HMM library"
+    python $DIR/parse_hmm_files.py $hmm $tmp/HMMs
+    echo "Done parsing HMM library"
+fi
+
+for hmmfile in $tmp/HMMs/*; do
+    hmm_acc=$(basename $hmmfile .hmm)
+    # clear hmmer output if it exists
+    if [[ -f $tmp/${base_fasta}_${hmm_acc}.hmmer ]]; then
+        rm $tmp/${base_fasta}_${hmm_acc}.hmmer
+    fi
+    echo "Fishing for reads with ${hmm_acc}"
+    for i in {1..6}
+    do
+        bash $DIR/hmmer3_pipeline_strand.sh $hmmfile $tmp/${base_fasta}.frame${i} $i >> $tmp/${base_fasta}_${hmm_acc}.hmmer
+    done
+    echo "Assembling ${hmm_acc}"
+    python $DIR/assembler.py $tmp/${base_fasta}_${hmm_acc}.hmmer $fasta ${hmm_acc} $t $d $out 
 done
 #rm -r $tmp/HMMs
 #rm $tmp/${base_fasta}.frame?
